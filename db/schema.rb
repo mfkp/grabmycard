5a979453bb68d4bb2d80fdbb67f3d459f5bcbe52
kjulius <kylepowers@gmail.com>
2009-08-30 18:30:00 -0500

    adding card model, starting card view

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090830221233) do
+ActiveRecord::Schema.define(:version => 20090830230903) do
+
+  create_table "cards", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+  end
 
   create_table "user_sessions", :force => true do |t|
     t.string   "username"
