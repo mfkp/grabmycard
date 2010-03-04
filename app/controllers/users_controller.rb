598f1f33596b10f964285cb093a7039e6e23c8b7
Kyle Powers <kylepowers@gmail.com>
2010-03-03 20:33:53 -0600

    redirecting updated user to cards page instead of root_url

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,7 +50,7 @@ class UsersController < ApplicationController
     @user = current_user
     if @user.update_attributes(params[:user])
       flash[:notice] = "Successfully updated profile."
-      redirect_to root_url
+      redirect_to cards_url
     else
       render :action => 'edit'
     end
